a38f9fe0e18e6c90b5763480ee47bdccad9dd3f9
Evgeniy Titov <falgore88@gmail.com>
2017-06-06 14:08:01 +0300

    fix

--- debug_panel/urls.py
+++ debug_panel/urls.py
@@ -5,11 +5,8 @@ These should not be loaded explicitly; It is used internally by the
 debug-panel application.
 """
 from .views import debug_data
+from django.conf.urls import url
 
-try:
-    from django.conf.urls import url
-except ImportError:  # django < 1.4
-    from django.conf.urls.defaults import url
 
 from django.conf.urls.i18n import i18n_patterns
 
